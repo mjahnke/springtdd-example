cc1f41e7841813022e74aa8629dafc519679b3f0
Bernd Ritter <ritter@decoit.de>
2013-09-03 17:08:45 +0200

    Testdriven komplett mit einheitlicher Konfiguration

--- src/main/java/de/decoit/springtdd/controller/HelloController.java
+++ src/main/java/de/decoit/springtdd/controller/HelloController.java
@@ -4,6 +4,7 @@
  */
 package de.decoit.springtdd.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
+    @Value("${farbe}") private String farbe;
+    @Value("${wetter}") private String wetter;
+    
     @RequestMapping(value="/index.html")
     public String printHello(final ModelMap map) {
-        map.put("farbe", "grün");
-        map.put("wetter", "sonnig");
+        map.put("farbe", farbe);
+        map.put("wetter", wetter);
         return "index";
     }
 }
