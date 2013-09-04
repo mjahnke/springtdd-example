fedc3b14cc2c050901751c5dfac369cd2b67a524
Bernd Ritter <ritter@decoit.de>
2013-09-04 13:40:01 +0200

    Leerer Skeleton

--- src/main/java/de/decoit/springtdd/controller/HelloController.java
+++ src/main/java/de/decoit/springtdd/controller/HelloController.java
@@ -4,50 +4,9 @@
  */
 package de.decoit.springtdd.controller;
 
-import de.decoit.springtdd.controller.form.Hello;
-import javax.validation.Valid;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-
-    @Value("${farbe}") private String farbe;
-    @Value("${wetter}") private String wetter;
-    
-    @RequestMapping("/index.html")
-    public String printHello(final ModelMap map) {
-        map.put("farbe", farbe);
-        map.put("wetter", wetter);
-        return "index";
-    }
-    
-    @RequestMapping("/sayhello/{name}")
-    public String sayhello(final ModelMap map, @PathVariable("name") final String name) {
-        map.put("name", name);
-        return "hello";
-    }
-    
-    @RequestMapping("/helloform.html")
-    public ModelAndView helloForm() {
-        return new ModelAndView("helloform", "hello", new Hello());
-    }
-    
-    @RequestMapping(value="helloform.html", method = RequestMethod.POST)
-    public ModelAndView helloFormSubmit(@Valid @ModelAttribute("hello") Hello helloCmd, final BindingResult result, final ModelMap map) {
-        if (result.hasErrors()) {
-            return new ModelAndView("helloform");
-        }
-
-        map.put("name", helloCmd.getName());        
-        
-        return new ModelAndView("hello", map);
-    }    
+   
 }
--- src/main/java/de/decoit/springtdd/controller/form/Hello.java
+++ src/main/java/de/decoit/springtdd/controller/form/Hello.java
@@ -4,25 +4,10 @@
  */
 package de.decoit.springtdd.controller.form;
 
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.NotEmpty;
-
 /**
  *
  * @author britter
  */
 public class Hello {
-    
-    @NotEmpty
-    @Length(max=100)
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
     
 }
